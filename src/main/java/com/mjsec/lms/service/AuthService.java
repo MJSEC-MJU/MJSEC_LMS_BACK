a60fce36d565a55a3cdd79eeb57cf5259d5fc875
minsoo0506 <mspark010506@gmail.com>
2025-07-21 18:25:54 +0900

    feat(signup): add verification using regex in checkStudentNumber method

--- src/main/java/com/mjsec/lms/service/AuthService.java
+++ src/main/java/com/mjsec/lms/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.mjsec.lms.service;
 
+import com.mjsec.lms.exception.RestApiException;
 import com.mjsec.lms.repository.PendingUserRepository;
 import com.mjsec.lms.repository.UserRepository;
+import com.mjsec.lms.type.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,12 @@ public class AuthService {
      */
     public Boolean checkStudentNumber(Long studentNumber){
 
+        // 학번이 8자리 숫자인지 체크
+        if(!String.valueOf(studentNumber).matches("^\\d{8}$")){
+            throw new RestApiException(ErrorCode.INVALID_STUDENT_NUMBER);
+        }
+
+        // 학번이 회원가입 승인 대기 중인 테이블 & 유저 테이블에 등록되어 있는지 확인
         if(pendingUserRepository.existsByStudentNumber(studentNumber)) return false;
         if(userRepository.existsByStudentNumber(studentNumber)) return false;
 
